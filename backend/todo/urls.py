e25d33dc57f35c27ec4dc83ef4aae3f7c1ddfd37
Suleyman Aliyev <36112126+suleyman735@users.noreply.github.com>
2023-09-14 04:37:01 +0400

    done crud system without api

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
      path('', views.index, name='index'),
      path('create/', views.create, name='create'),
+     path('edit/<int:id>/', views.edit, name='edit'),
+     path('update/<int:id>', views.update, name='update'),
+     path('delete/<int:id>', views.destroy, name='destroy'),
      
    
 ]
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -6,7 +6,16 @@ from .forms import TodoForm
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html',)
+    
+    todoComplete = ToDoItem.objects.filter(done="True")
+    todoRunning = ToDoItem.objects.filter(done="False")
+   
+    context = {'todoComplete': todoComplete,
+               'todoRunning':todoRunning}
+    return render(request, 'index.html',context)
+
+
+
 
 def create(request):
     if request.method == "POST":
@@ -14,13 +23,48 @@ def create(request):
         print(form)
         if form.is_valid():
             try:
-                # cd = form.cleaned_data
-                # print(cd)
+    
                 form.save()
-                # return redirect("/create/")
+      
             except:
                 print('e')
     else:
         form = TodoForm()
     
     return render(request, 'create.html',{'form':form})
+
+
+def edit(request, id):  
+    todoEdit = ToDoItem.objects.get(id=id)  
+    return render(request,'edit.html', {'todoEdit':todoEdit}) 
+
+def update(request, roll): 
+    # title = request.POST.get('title')
+    # description = request.POST.get('description')
+    # created_date = request.POST.get('created_date')
+    # due_date = request.POST.get('due_date')
+    # done = request.POST.get('done')
+    
+    # std= ToDoItem.objects.get(pk=roll)
+    
+    # std.title=title
+    # std.description = description
+    # std.created_date = created_date
+    # std.due_date = due_date
+    # std.done= done
+    
+    # std.save()
+    # print('hello') 
+    todoEdit = ToDoItem.objects.get(id=id)  
+    form = TodoForm(request.POST, instance = todoEdit)  
+        
+    if form.is_valid():  
+        form.save() 
+        print(form) 
+        return redirect("/")  
+    return render(request, 'edit.html', {'todoEdit': todoEdit})
+
+def destroy(request, id):  
+    todoItem = ToDoItem.objects.get(id=id)  
+    todoItem.delete()  
+    return redirect("/")
