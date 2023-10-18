2110fbedb0c42bf91f7bcd351b693872a6145669
Suleyman Aliyev <36112126+suleyman735@users.noreply.github.com>
2023-10-18 17:07:11 +0200

    update  section
    update ready except check

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
      path('login/', views.login, name='login'),
      path('create/', views.create, name='create'),
      
-     path('edit/<int:id>/', views.edit, name='edit'),
+     # path('edit/<int:id>/', views.edit, name='edit'),
      path('update/<int:id>/', views.update, name='update'),
      path('delete/<int:id>', views.destroy, name='destroy'),
      
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,9 +1,9 @@
 from django.db import IntegrityError
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect,get_object_or_404, render
 from .models import *
 from datetime import datetime
 
-from .forms import TodoForm
+
 
 # Create your views here.
 
@@ -32,37 +32,30 @@ def create(request):
 
         print(e)
     return render(request,'create.html')
-def edit(request, id):  
-    todoEdit = ToDoItem.objects.get(id=id)  
-    return render(request,'edit.html', {'todoEdit':todoEdit}) 
+# def edit(request, id):
+#     todoEdit = ToDoItem.objects.get(id=id)
+#     return render(request,'edit.html', {'todoEdit':todoEdit})
 
-def update(request, roll): 
-    # title = request.POST.get('title')
-    # description = request.POST.get('description')
-    # created_date = request.POST.get('created_date')
-    # due_date = request.POST.get('due_date')
-    # done = request.POST.get('done')
-    
-    # std= ToDoItem.objects.get(pk=roll)
-    
-    # std.title=title
-    # std.description = description
-    # std.created_date = created_date
-    # std.due_date = due_date
-    # std.done= done
-    
-    # std.save()
-    # print('hello') 
-    todoEdit = ToDoItem.objects.get(id=id)  
-    form = TodoForm(request.POST, instance = todoEdit)  
-        
-    if form.is_valid():  
-        form.save() 
-        print(form) 
-        return redirect("/") 
-    else:
-        print(form.errors) 
-    return render(request, 'edit.html', {'todoEdit': todoEdit})
+def update(request, id):
+    item = get_object_or_404(ToDoItem, pk=id)
+
+    if request.method=='POST':
+        title = request.POST['title']
+        description = request.POST['description']
+        created_date = request.POST['created_date']
+        # strDatetime = datetime.strptime(created_date,'%Y-%m-%dT%H:%M')
+        due_date = request.POST['due_date']
+        is_checked = request.POST.get('is_checked') == 'on'
+        print(is_checked)
+        item.title = title
+        item.description = description
+        item.created_date = created_date
+        item.due_date = due_date
+        item.done = is_checked
+        item.save()
+
+    return render(request, 'edit.html', {'todoEdit': item})
+
 
 def destroy(request, id):  
     todoItem = ToDoItem.objects.get(id=id)  
